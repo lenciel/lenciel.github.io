9300824c6d848120f7fa3a36e0aa338fc62f0bd9
lenciel <lenciel@gmail.com>
2017-04-04 22:42:17 +0800

    add a script to prepare deploy artifacts

---
+++ prepare.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+git checkout source
+git pull
+rake prepare_deploy
+#git checkout master && mv _site .site && rm -rf * && mv .site/* . && rm -rf .site
+#git add .; \
+#git commit -am "Update blog content update $$(date +%Y-%m-%d)"; \
+#git push origin master
